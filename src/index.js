e70d49169392bc6bac42d92e3c0a9c0778c9a5db
Fernando L.C <fer.lc.developer@gmail.com>
2022-08-16 15:17:28 -0500

    [FEAT]-[APP]-Example for style

--- src/index.js
+++ src/index.js
@@ -14,14 +14,22 @@ const AppWithCallbackAfterRender = () => {
   });
 
   return (
-    <div>
-      <h1> Welcome </h1>
-      <img src={imageReact} alt="React" />
-      <img src={imageRedux} alt="Redux" />
-      <img src={imageSass} alt="Sass" />
-      <img src={imagePrettier} alt="Prettier" />
-      <img src={imageEslint} alt="Eslint" />
-      <img src={imageWebpack} alt="Webpack" />
+    <div className="container">
+      <div className="sub-section">
+        <h1 className="title"> Welcome </h1>
+      </div>
+      <div className="sub-section">
+        <img src={imageReact} className="image image-animation" alt="React" />
+        <img src={imageRedux} className="image image-animation" alt="Redux" />
+        <img src={imageSass} className="image" alt="Sass" />
+        <img src={imagePrettier} className="image" alt="Prettier" />
+        <img src={imageEslint} className="image image-animation" alt="Eslint" />
+        <img
+          src={imageWebpack}
+          className="image image-animation"
+          alt="Webpack"
+        />
+      </div>
     </div>
   );
 };
