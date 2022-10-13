23a64ed8b044d01f7ecf68b807903be0fb9d63cc
Fernando.LC <fer.lc.developer@gmail.com>
2022-10-13 00:42:48 -0500

    fix(persist): update redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,24 @@
-import persistState from 'redux-localstorage'
+import adapter from 'redux-localstorage/lib/adapters/localStorage'
+import filter from 'redux-localstorage-filter'
 import reducers from 'Config/reducers.js'
 import { state } from 'Config/state.js'
 import thunk from 'redux-thunk'
 import { applyMiddleware, compose, createStore } from 'redux'
+import { KEY_FILTER_PERSIST, KEYS_STORAGE } from 'Constants/constantStore.js'
+import persistState, { mergePersistedState } from 'redux-localstorage'
 
 const middleWareGeneral = store => next => action => next(action)
 
+const reducer = compose(mergePersistedState())(reducers)
+
+const storage = compose(filter(KEY_FILTER_PERSIST))(adapter(sessionStorage))
+
 const enhancer = compose(
   applyMiddleware(thunk, middleWareGeneral),
-  persistState(['title', 'theme'], 'forever'),
+  persistState(storage, KEYS_STORAGE),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-const store = createStore(reducers, state, enhancer)
+const store = createStore(reducer, state, enhancer)
 
 export { store }
