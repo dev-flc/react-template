ed517728481aa40caf6815330b670c6bc9c2fd4c
Ferch01992 <fer.lc.developer@gmail.com>
2023-09-26 07:39:09 -0600

    feat(webpack): Refactor webpack

--- webpack.common.js
+++ webpack.common.js
@@ -5,6 +5,85 @@ const WebpackBar = require('webpackbar')
 
 const TIME = new Date().getTime()
 const { VERSION_APP, NAME_APP } = process.env
+const SRC_DIR = path.resolve(__dirname, './src')
+
+function rulesJS() {
+  return {
+    exclude: [path.resolve(__dirname, 'node_modules')],
+    include: [path.resolve(__dirname, 'src')],
+    test: /\.[jt]sx?$/,
+    use: [
+      {
+        loader: require.resolve('babel-loader'),
+        options: {
+          plugins: ['@babel/plugin-proposal-class-properties'],
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+}
+
+function rulesCSS() {
+  return {
+    test: /\.(sass|css|scss)$/,
+    use: ['style-loader', 'css-loader', 'sass-loader']
+  }
+}
+
+function rulesASSETS() {
+  return [
+    {
+      generator: {
+        filename: `assets/images/[name].${VERSION_APP}.${TIME}.[hash][ext][query]`
+      },
+      test: /\.(png|jpg|gif|svg)(\?[\s\S]+)?$/,
+      type: 'asset/resource'
+    },
+    {
+      generator: {
+        filename: `assets/fonts/[name].${VERSION_APP}.${TIME}.[hash][ext][query]`
+      },
+      test: /\.(ttf|eot|otf|woff2|woff)(\?[\s\S]+)?$/,
+      type: 'asset/resource'
+    }
+  ]
+}
+
+const output = {
+  chunkFilename: `js/[id].${VERSION_APP}.${TIME}.[contenthash].js`,
+  chunkLoadTimeout: 30000,
+  clean: true,
+  filename: `js/[name].${VERSION_APP}.${TIME}.[contenthash].js`,
+  path: path.resolve(__dirname, 'public'),
+  pathinfo: true,
+  publicPath: '/',
+  uniqueName: NAME_APP
+}
+
+function createAlias(name, relativePath) {
+  return {
+    [name]: path.resolve(SRC_DIR, relativePath)
+  }
+}
+
+const alias = {
+  ...createAlias('Actions', './store/actions'),
+  ...createAlias('Components', './components'),
+  ...createAlias('Config', './store/config'),
+  ...createAlias('Constants', './constants'),
+  ...createAlias('Containers', './containers'),
+  ...createAlias('Fonts', './assets/fonts'),
+  ...createAlias('Hook', './hook'),
+  ...createAlias('Images', './assets/images'),
+  ...createAlias('Reducers', './store/reducers'),
+  ...createAlias('Routers', './routers'),
+  ...createAlias('Selectors', './store/selectors'),
+  ...createAlias('Store', './store'),
+  ...createAlias('Styles', './assets/styles'),
+  ...createAlias('Utils', './utils'),
+  ...createAlias('View', './view')
+}
 
 module.exports = {
   entry: {
@@ -16,59 +95,13 @@ module.exports = {
     }
   },
 
-  module: {
-    rules: [
-      {
-        exclude: [path.resolve(__dirname, 'node_modules')],
-        include: [path.resolve(__dirname, 'src')],
-        test: /\.[jt]sx?$/,
-        use: [
-          {
-            loader: require.resolve('babel-loader'),
-            options: {
-              plugins: ['@babel/plugin-proposal-class-properties'],
-              presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(sass|css|scss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        generator: {
-          filename: `assets/images/[name].${VERSION_APP}.${TIME}.[hash][ext][query]`
-        },
-        test: /\.(png|jpg|gif|svg)(\?[\s\S]+)?$/,
-        type: 'asset/resource'
-      },
-      {
-        generator: {
-          filename: `assets/fonts/[name].${VERSION_APP}.${TIME}.[hash][ext][query]`
-        },
-        test: /\.(ttf|eot|otf|woff2|woff)(\?[\s\S]+)?$/,
-        type: 'asset/resource'
-      }
-    ]
-  },
+  module: { rules: [rulesJS(), rulesCSS(), ...rulesASSETS()] },
 
-  output: {
-    chunkFilename: `js/[id].${VERSION_APP}.${TIME}.[contenthash].js`,
-    chunkLoadTimeout: 30000,
-    clean: true,
-    filename: `js/[name].${VERSION_APP}.${TIME}.[contenthash].js`,
-    path: path.resolve(__dirname, 'public'),
-    pathinfo: true,
-    publicPath: '/',
-    uniqueName: NAME_APP
-  },
+  output,
 
   plugins: [
     new CleanWebpackPlugin(),
-
     new WebpackBar({ color: '#ff4081' }),
-
     new HtmlWebpackPlugin({
       cache: false,
       favicon: path.resolve(__dirname, './src/assets/images/logo.svg'),
@@ -80,37 +113,7 @@ module.exports = {
   ],
 
   resolve: {
-    alias: {
-      /* Store */
-      Actions: path.resolve(__dirname, './src/store/actions'),
-      /* Componets */
-      Components: path.resolve(__dirname, './src/components'),
-      /* Store */
-      Config: path.resolve(__dirname, './src/store/config'),
-      /* constants */
-      Constants: path.resolve(__dirname, './src/constants'),
-      /* Containers */
-      Containers: path.resolve(__dirname, './src/containers'),
-      /* assets */
-      Fonts: path.resolve(__dirname, './src/assets/fonts'),
-      /* Hook */
-      Hook: path.resolve(__dirname, './src/hook'),
-      /* assets */
-      Images: path.resolve(__dirname, './src/assets/images'),
-      /* Store */
-      Reducers: path.resolve(__dirname, './src/store/reducers'),
-      /* Routers */
-      Routers: path.resolve(__dirname, './src/routers'),
-      /* Store */
-      Selectors: path.resolve(__dirname, './src/store/selectors'),
-      Store: path.resolve(__dirname, './src/store'),
-      /* assets */
-      Styles: path.resolve(__dirname, './src/assets/styles'),
-      /* Utils */
-      Utils: path.resolve(__dirname, './src/utils'),
-      /* View */
-      View: path.resolve(__dirname, './src/view')
-    },
+    alias,
     extensions: ['.js', '.json']
   }
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -6,71 +6,65 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const webpack = require('webpack')
 
-const date = new Date()
+const DATE = new Date()
+const TIME = DATE.getTime()
 
-const time = date.getTime()
+const cssRule = {
+  resolve: { extensions: ['.scss', '.css'] },
+  test: /\.(sa|sc|c)ss$/,
+  use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+}
+
+const commonCacheGroup = {
+  chunks: 'initial',
+  minChunks: 2,
+  name: `chunk-common.${TIME}`,
+  priority: -30,
+  reuseExistingChunk: true
+}
+
+const vendorCacheGroup = {
+  chunks: 'all',
+  maxSize: 30000,
+  name: `common.${TIME}`,
+  test: /[\\/]node_modules[\\/]/
+}
+
+const output = {
+  chunkFilename: `js/${process.env.VERSION_APP}.[name].[id].${TIME}.[contenthash].[chunkhash].min.js`,
+  filename: `js/${process.env.VERSION_APP}.[name].${TIME}.[contenthash].min.js`,
+  path: path.resolve(__dirname, 'public'),
+  publicPath: '/'
+}
+
+const plugins = [
+  new webpack.ids.HashedModuleIdsPlugin({}),
+  new CompressionPlugin({
+    algorithm: 'gzip',
+    deleteOriginalAssets: true,
+    exclude: ['service-worker', 'firebase-messaging-sw', 'precache-manifest'],
+    filename: '[path][base].gz',
+    minRatio: Number.MAX_SAFE_INTEGER,
+    test: /\.js$|\.png$|\.svg$|\.css$|\.eot?.+$|\.ttf?.+$/,
+    threshold: 0
+  }),
+  new MiniCssExtractPlugin({
+    filename: `assets/css/${process.env.VERSION_APP}.[name].${TIME}.[contenthash].min.css`
+  })
+]
 
 module.exports = merge(common, {
   devtool: false,
-
   mode: 'production',
-
-  module: {
-    rules: [
-      {
-        resolve: { extensions: ['.scss', '.css'] },
-        test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-
+  module: { rules: [cssRule] },
   optimization: {
     minimizer: [new CssMinimizerPlugin()],
     runtimeChunk: 'single',
     splitChunks: {
-      cacheGroups: {
-        common: {
-          chunks: 'initial',
-          minChunks: 2,
-          name: `chunk-common.${time}`,
-          priority: -30,
-          reuseExistingChunk: true
-        },
-        vendor: {
-          chunks: 'all',
-          maxSize: 30000,
-          name: `common.${time}`,
-          test: /[\\/]node_modules[\\/]/
-        }
-      }
+      cacheGroups: { common: commonCacheGroup, vendor: vendorCacheGroup }
     }
   },
-
-  output: {
-    chunkFilename: `js/${process.env.VERSION_APP}.[name].[id].${time}.[contenthash].[chunkhash].min.js`,
-    filename: `js/${process.env.VERSION_APP}.[name].${time}.[contenthash].min.js`,
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/'
-  },
-
-  plugins: [
-    new webpack.ids.HashedModuleIdsPlugin({}),
-
-    new CompressionPlugin({
-      algorithm: 'gzip',
-      deleteOriginalAssets: true,
-      exclude: ['service-worker', 'firebase-messaging-sw', 'precache-manifest'],
-      filename: '[path][base].gz',
-      minRatio: Number.MAX_SAFE_INTEGER,
-      test: /\.js$|\.png$|\.svg$|\.css$|\.eot?.+$|\.ttf?.+$/,
-      threshold: 0
-    }),
-
-    new MiniCssExtractPlugin({
-      filename: `assets/css/${process.env.VERSION_APP}.[name].${time}.[contenthash].min.css`
-    })
-  ],
-
+  output,
+  plugins,
   stats: 'minimal'
 })
